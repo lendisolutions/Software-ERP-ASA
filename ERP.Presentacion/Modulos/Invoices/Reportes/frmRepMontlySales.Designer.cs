e114d1f1b739ae97822061f738bec65a24b1929c
lendisolutions <46729497+lendisolutions@users.noreply.github.com>
2019-04-22 16:57:25 -0500

    22042019 1657

---
+++ ERP.Presentacion/Modulos/Invoices/Reportes/frmRepMontlySales.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ERP.Presentacion.Modulos.Invoices.Reportes
+{
+    partial class frmRepMontlySales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRepMontlySales));
+            this.btnExport = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCancelar = new DevExpress.XtraEditors.SimpleButton();
+            this.txtPeriodo = new DevExpress.XtraEditors.SpinEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.cboMes = new DevExpress.XtraEditors.LookUpEdit();
+            this.labelControl13 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPeriodo.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cboMes.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.ImageOptions.Image = global::ERP.Presentacion.Properties.Resources.excel_16x16;
+            this.btnExport.ImageOptions.ImageIndex = 1;
+            this.btnExport.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btnExport.Location = new System.Drawing.Point(240, 96);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(134, 28);
+            this.btnExport.TabIndex = 154;
+            this.btnExport.Text = "Export To Excel";
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.ImageOptions.Image")));
+            this.btnCancelar.ImageOptions.ImageIndex = 0;
+            this.btnCancelar.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(380, 96);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(87, 28);
+            this.btnCancelar.TabIndex = 153;
+            this.btnCancelar.Text = "Cancel";
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txtPeriodo
+            // 
+            this.txtPeriodo.EditValue = new decimal(new int[] {
+            2017,
+            0,
+            0,
+            0});
+            this.txtPeriodo.Location = new System.Drawing.Point(71, 22);
+            this.txtPeriodo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPeriodo.Name = "txtPeriodo";
+            this.txtPeriodo.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.txtPeriodo.Properties.DisplayFormat.FormatString = "n0";
+            this.txtPeriodo.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
+            this.txtPeriodo.Properties.MaxValue = new decimal(new int[] {
+            2100,
+            0,
+            0,
+            0});
+            this.txtPeriodo.Properties.MinValue = new decimal(new int[] {
+            2017,
+            0,
+            0,
+            0});
+            this.txtPeriodo.Size = new System.Drawing.Size(117, 22);
+            this.txtPeriodo.TabIndex = 156;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(18, 26);
+            this.labelControl3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(31, 16);
+            this.labelControl3.TabIndex = 155;
+            this.labelControl3.Text = "Year:";
+            // 
+            // cboMes
+            // 
+            this.cboMes.Location = new System.Drawing.Point(71, 49);
+            this.cboMes.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cboMes.Name = "cboMes";
+            this.cboMes.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cboMes.Properties.NullText = "";
+            this.cboMes.Size = new System.Drawing.Size(169, 22);
+            this.cboMes.TabIndex = 163;
+            // 
+            // labelControl13
+            // 
+            this.labelControl13.Location = new System.Drawing.Point(18, 52);
+            this.labelControl13.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl13.Name = "labelControl13";
+            this.labelControl13.Size = new System.Drawing.Size(40, 16);
+            this.labelControl13.TabIndex = 162;
+            this.labelControl13.Text = "Month:";
+            // 
+            // frmRepMontlySales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 135);
+            this.Controls.Add(this.cboMes);
+            this.Controls.Add(this.labelControl13);
+            this.Controls.Add(this.txtPeriodo);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnCancelar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmRepMontlySales";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Montly Sales Report";
+            this.Load += new System.EventHandler(this.frmRepMontlySales_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtPeriodo.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cboMes.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btnExport;
+        private DevExpress.XtraEditors.SimpleButton btnCancelar;
+        private DevExpress.XtraEditors.SpinEdit txtPeriodo;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        public DevExpress.XtraEditors.LookUpEdit cboMes;
+        private DevExpress.XtraEditors.LabelControl labelControl13;
+    }
+}
---
+++ ERP.Presentacion/Modulos/Invoices/Reportes/frmRepMontlySales.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq;
+using System.Security.Principal;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.IO;
+using DevExpress.XtraEditors;
+using DevExpress.XtraBars;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using DevExpress.XtraGrid.Columns;
+using ERP.Presentacion.Modulos.Otros;
+using ERP.Presentacion.Utils;
+using ERP.Presentacion;
+using ERP.BusinessEntity;
+using ERP.BusinessLogic;
+
+namespace ERP.Presentacion.Modulos.Invoices.Reportes
+{
+    public partial class frmRepMontlySales : DevExpress.XtraEditors.XtraForm
+    {
+        #region "Propiedades"
+
+        #endregion
+
+        #region "Eventos"
+
+        public frmRepMontlySales()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRepMontlySales_Load(object sender, EventArgs e)
+        {
+            txtPeriodo.EditValue = Parametros.intPeriodo;
+            BSUtils.LoaderLook(cboMes, CargarMes(), "Descripcion", "Id", false);
+            cboMes.EditValue = DateTime.Now.Month;
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        #region "Metodos"
+
+        private DataTable CargarMes()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id", Type.GetType("System.Int32"));
+            dt.Columns.Add("Descripcion", Type.GetType("System.String"));
+            DataRow dr;
+            dr = dt.NewRow();
+            dr["Id"] = 1;
+            dr["Descripcion"] = "ENERO";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 2;
+            dr["Descripcion"] = "FEBRERO";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 3;
+            dr["Descripcion"] = "MARZO";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 4;
+            dr["Descripcion"] = "ABRIL";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 5;
+            dr["Descripcion"] = "MAYO";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 6;
+            dr["Descripcion"] = "JUNIO";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 7;
+            dr["Descripcion"] = "JULIO";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 8;
+            dr["Descripcion"] = "AGOSTO";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 9;
+            dr["Descripcion"] = "SEPTIEMBRE";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 10;
+            dr["Descripcion"] = "OCTUBRE";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 11;
+            dr["Descripcion"] = "NOVIEMBRE";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr["Id"] = 12;
+            dr["Descripcion"] = "DICIEMBRE";
+            dt.Rows.Add(dr);
+
+            return dt;
+        }
+
+        #endregion
+
+
+    }
+}
